2f16e4355dd83b90b18009edae186af7d4c34ec0
Shatgul <shatgul@shatgul>
2022-01-19 02:27:09 -0500

    update backend code

--- controllers/profile_controller.py
+++ controllers/profile_controller.py
@@ -25,10 +25,14 @@ def insert():
         return redirect('/login')
     # INSERT INTO DB
     insert_profile(
+        int(session.get("user_id")),
         request.form.get("name"),
+        int(request.form.get("num_of_kids")),
         request.form.get("location"),
         request.form.get("skills"),
-        request.form.get("image_url")
+        request.form.get("image_url"),
+        request.form.get("experience"),
+        request.form.get("availability")
     )
 
     return redirect('/')
@@ -53,12 +57,16 @@ def update(id):
     if not session.get('user_id'):
         return redirect('/login')
     name = request.form.get("name")
+    num_of_kids = request.form.get("num_of_kids")
     location = request.form.get("location")
-    description = request.form.get("skills")
     image_url = request.form.get("image_url")
+    skills = request.form.get("skills")
+    experience = request.form.get("experience")
+    availability = request.form.get("availability")
 
     # UPDATE
-    update_profile(id, name, location, description, image_url)
+    update_profile(id, name, num_of_kids, location, image_url,
+                   skills, experience, availability)
 
     return redirect('/')
 
--- models/profile.py
+++ models/profile.py
@@ -11,12 +11,16 @@ import database
 # INSERT PROFILE INTO DB
 
 
-def insert_profile(name, location, skills, image_url):
-    database.sql_write("INSERT into profiles (name, location, skills, image_url) VALUES (%s, %s, %s, %s);", [
+def insert_profile(user_id, name, num_of_kids, location, skills, image_url, experience, availability):
+    database.sql_write("INSERT into profiles (user_id, name, num_of_kids, location, skills, image_url, experience, availability) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", [
+        user_id,
         name,
+        num_of_kids,
         location,
         skills,
-        image_url
+        image_url,
+        experience,
+        availability
     ])
 
 # SELECT PROFILE FROM DB
@@ -24,7 +28,7 @@ def insert_profile(name, location, skills, image_url):
 
 def get_profile(id):
     results = database.sql_select(
-        'SELECT id, name, location, image_url, skills FROM profiles WHERE id = %s', [id])
+        'SELECT id, user_id, name, num_of_kids, location, image_url, skills, experience, availability FROM profiles WHERE id = %s', [id])
     result = results[0]
     return result
 
@@ -33,18 +37,21 @@ def get_profile(id):
 
 def get_all_profile():
     results = database.sql_select(
-        "SELECT id, name, location, image_url, skills FROM profiles", [])
+        "SELECT id, user_id, name, num_of_kids, location, image_url, skills, experience, availability FROM profiles", [])
     return results
 
 # UPDATE PROFILE IN DB
 
 
-def update_profile(id, name, location, skills, image_url):
-    database.sql_write("UPDATE profiles set name = %s, location = %s, skills = %s, image_url = %s WHERE id = %s", [
+def update_profile(id, name, num_of_kids, location, image_url, skills, experience, availability):
+    database.sql_write("UPDATE profiles set name = %s, num_of_kids =%s, location = %s, image_url = %s, skills = %s, experience =%s, availability=%s WHERE id = %s", [
         name,
+        num_of_kids,
         location,
-        skills,
         image_url,
+        skills,
+        experience,
+        availability,
         id
     ])
 
